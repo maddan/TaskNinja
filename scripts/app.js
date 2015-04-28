7a6d1d501e2217dc5b9a59a7605eb31ee48aa9f4
Dan dan <dan@dan.com>
2015-04-28 15:33:16 -0400

    connected with Firebase in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -14,13 +14,16 @@ var app = angular
 				templateUrl: 'views/main.html'
 			})
 			.when('/post', {
-				templateUrl: 'views/post.html'
+				templateUrl: 'views/post.html',
+				controller: 'TaskController'
 			})
-			.when('/edit', {
-				templateUrl: 'views/edit.html'
+			.when('/edit/:taskId', {
+				templateUrl: 'views/edit.html',
+				controller: 'TaskController'
 			})
 			.when('/browse', {
-				templateUrl: 'views/browse.html'
+				templateUrl: 'views/browse.html',
+				controller: 'TaskController'
 			})
 			.otherwise({
 				redirectTo: '/'
